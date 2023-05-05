578efd64d5e9b9583cef283b2f470aace04c2bf1
svenvanos <svenvanos@gmail.com>
2023-05-05 12:06:53 +0200

    finished lesson 5

--- Work/stock.py
+++ Work/stock.py
@@ -1,6 +1,7 @@
 # stock.py
 
 class Stock:
+    __slots__ = ('name','_shares','price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -8,23 +9,21 @@ class Stock:
         
     def __repr__(self):
         return f"Stock({self.name!r}, {self.shares!r}, {self.price!r})"
+       
+    @property
+    def shares(self):
+        return self._shares
 
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Expected int")
+        self._shares = value
+        
+    @property
     def cost(self):
         return self.shares * self.price
         
     def sell(self, nshares):
         self.shares -= nshares
         
-class MyStock(Stock):
-    def __init__(self, name, shares, price, factor):
-        # Check the call to "super" and "__init__"
-        super().__init__(name, shares, price)
-        self.factor = factor
-
-    def panic(self):
-        self.sell(self.shares)
-        
-    def cost(self):
-        # Check the call to "super"
-        actual_cost = super().cost()
-        return self.factor * actual_cost
